ca6ba345370103ca02900816b272f276e3881fd4
Darshanbc <darshan01075@gmail.com>
2019-06-16 00:30:41 +0530

    initial commit

---
+++ src/main/java/com/code_harden/kafka/simple/consumer/App.java
@@ -0,0 +1,13 @@
+package com.code_harden.kafka.simple.consumer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
